b293c4e08ceac3e76f196293551836c833a65ffc
debasisb <debasisb@hawaii.edu>
2024-02-21 03:28:37 +0000

    An extra line

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 // write some code to print "Hello World" to the console below
 console.log("Hello World");
 console.log("hi");
+// This is a single-line comment
+// write some code to print "Hello World" to the console below
+console.log("Hello World");
+console.log("hi");
+consolu.log("Hello World");
