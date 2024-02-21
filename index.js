f118d9a71feefdc78210af666a0c7da83aa3ac5e
debasisb <debasisb@hawaii.edu>
2024-02-21 03:29:18 +0000

    New code

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ console.log("hi");
 // write some code to print "Hello World" to the console below
 console.log("Hello World");
 console.log("hi");
-consolu.log("Hello World");
+consolu.log("Hello World");// This is a single-line comment
+// write some code to print "Hello World" to the console below
+console.log("Hello World");
+console.log("hi");
